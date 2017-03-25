210726c4d32fc362094b4efe5be1475a135b4b58
Rodrigo Filomeno <rm.filomeno@gmail.com>
2017-03-25 11:40:10 -0300

    first commit

---
+++ ADS.Manha01.Sabado.Presentation.ConsoleApp/Templates/Blog.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using ADS.Manha01.Sabado.Presentation.ConsoleApp.Templates;
+ 
+namespace ADS.Manha01.Sabado.Presentation.ConsoleApp
+{
+    public class Blog
+    {
+		public Guid BlogId { get; set; }
+    }
+}
---
+++ ADS.Manha01.Sabado.Presentation.ConsoleApp/Templates/Tag.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using ADS.Manha01.Sabado.Presentation.ConsoleApp.Templates;
+ 
+namespace ADS.Manha01.Sabado.Presentation.ConsoleApp
+{
+    public class Tag
+    {
+		public Guid TagId { get; set; }
+    }
+}
